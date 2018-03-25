39fe0470bf1518b60e59874c3bb535382e30b0f8
Muhammad Muhajir <muhajirframe@gmail.com>
2018-03-25 14:57:20 +0700

    add styled components and remove logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,29 +10,18 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <h2>Logo here</h2>
+            {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
+        <a className="navbar-item" href="http://mkplxix.net">
+          Home
         </a>
+        <Link className="navbar-item" to="/">
+          Blog Posts
+        </Link>
       </div>
     </div>
   </nav>
